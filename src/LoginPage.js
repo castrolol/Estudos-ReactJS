1cd92c788eb19e60b8f673901f8d8aabd7386da2
Willian Andrade <willian.santos.andra@outlook.com>
2020-02-09 15:27:43 -0400

    alterações no layout da tela de login com a utilização de flexbox

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,24 +4,20 @@ import './LoginPage.css';
 export default function LoginPage() {
     return (
         <>
+        <h1>Hello World</h1>
         
-        <div className="flex-container">
-            <h1>Hello World</h1>
-        </div>
-
-        <div className="flex-container">
-            <form action="">
-                <label htmlFor="userInput">
-                    Username <input type="text" id="userInput"  name="username" />
-                </label>
-
-                <label htmlFor="passInput">
-                    Password <input type="password" id="passInput"  name="password" />
-                </label>
-
-                <input type="submit" value="Login" />
-            </form>
-        </div>
+        <form action="" className="flex-container">
+            <label htmlFor="userInput" id="form-layout">
+                Username <input type="text" id="userInput"  name="username" />
+            </label>
+        
+            <label htmlFor="passInput" id="form-layout">
+                Password <input type="password" id="passInput"  name="password" />
+            </label>
+        
+            <input type="submit" value="Login" />
+        </form>
         </>
     )
 }
+
