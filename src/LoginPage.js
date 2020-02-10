318391361fb5ca8aa1282a7c61f8189b3758f0d5
castrolol <luan.castro@nuvem.net>
2020-02-09 23:00:40 -0400

    Adiciona regras de flexbox

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,19 +4,26 @@ import './LoginPage.css';
 export default function LoginPage() {
     return (
         <>
-        <h1>Hello World</h1>
-        
-        <form action="" className="flex-container">
-            <label htmlFor="userInput" id="form-layout">
-                Username <input type="text" id="userInput"  name="username" />
-            </label>
-        
-            <label htmlFor="passInput" id="form-layout">
-                Password <input type="password" id="passInput"  name="password" />
-            </label>
-        
-            <input type="submit" value="Login" />
-        </form>
+            <div className="row">
+                <div className="logo">
+                    <h1>Hello World</h1>
+                </div>
+
+                <div className="form">
+                    <form action=""  >
+                        <h1>Hellow World Login</h1>
+                        <label htmlFor="userInput" id="form-layout">
+                            Username <input type="text" id="userInput" name="username" />
+                        </label>
+
+                        <label htmlFor="passInput" id="form-layout">
+                            Password <input type="password" id="passInput" name="password" />
+                        </label>
+
+                        <input type="submit" value="Login" />
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
